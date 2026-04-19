524c8e33abf6f52a5a1b8683f79d61380c010c60
Dongdarealest <107338168+Dongdarealest@users.noreply.github.com>
2026-04-19 22:35:13 +0700

    Update welcometest.js

--- welcometest.js
+++ welcometest.js
@@ -7,6 +7,6 @@ if (!$persistentStore.read("dongda_welcome")) {
       "open-url": "https://raw.githubusercontent.com/Dongdarealest/DONGUNLOCK/refs/heads/main/welcome.js"
     }
   );
-  $persistentStore.write("1", "dongda_welcome");
+  $persistentStore.write("100", "dongda_welcome");
 }
 $done({});
