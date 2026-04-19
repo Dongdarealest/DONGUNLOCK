74a120b1be76f574317ed71f51560b60e65c91fd
Dongdarealest <107338168+Dongdarealest@users.noreply.github.com>
2026-04-19 22:55:46 +0700

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -1,2 +1,13 @@
-$notification.post("DONGUNLOCK", "Kích hoạt thành công", "Chào mừng bạn đã sử dụng cấu hình của Dongdarealest!");
-$done({});
+let time = new Date().toLocaleTimeString('vi-VN');
+let hour = new Date().getHours();
+let greeting = "Chào bạn, ";
+if (hour < 12) greeting = "Chào buổi sáng 🌅, ";
+else if (hour < 18) greeting = "Chào buổi chiều ☕, ";
+else greeting = "Chào buổi tối 🌙, ";
+
+$done({
+  title: " DONGDAREALEST",
+  content: greeting + "\nHệ thống Premium đang hoạt động ổn định.\nCập nhật lần cuối: " + time,
+  icon: "checkmark.shield.fill",
+  "icon-color": "#147efb"
+});
